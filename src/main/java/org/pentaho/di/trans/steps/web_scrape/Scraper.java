8d4a3943a470483b80171c20e0e37e7831942419
Varsken Pitiakhsh <schal13@freeuni.edu.ge>
2018-01-18 02:11:25 +0400

    rough draft with the help of EC rest api.

--- src/main/java/org/pentaho/di/trans/steps/web_scrape/Scraper.java
+++ src/main/java/org/pentaho/di/trans/steps/web_scrape/Scraper.java
@@ -1,19 +1,27 @@
 package org.pentaho.di.trans.steps.web_scrape;
 
+import com.eclipsesource.json.Json;
 import com.eclipsesource.json.JsonArray;
 import com.eclipsesource.json.JsonObject;
-import org.eclipse.jetty.util.ajax.JSON;
+import com.eclipsesource.json.JsonValue;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.core.row.RowDataUtil;
 import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransMeta;
 import org.pentaho.di.trans.step.*;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class Scraper extends BaseStep implements StepInterface {
 
@@ -60,7 +68,44 @@ public class Scraper extends BaseStep implements StepInterface {
 
         String urlFieldName = scraperMeta.getUrlFieldName();
         String url = getInputRowMeta().getString(r, getInputRowMeta().indexOfValue(urlFieldName) );
-        Document doc;
+
+        String projectsJsonFromApi = null;
+        try {
+            String a = "https://ec.europa.eu/eipp/desktop/en/data/projects.json";
+            URLConnection connection = new URL(a).openConnection();
+            connection
+                    .setRequestProperty("User-Agent",
+                            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+            connection.connect();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("UTF-8")));
+
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            projectsJsonFromApi = sb.toString();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+
+        JsonObject apiResponse = Json.parse(projectsJsonFromApi).asObject();
+        List<String> projectUrls = new ArrayList<String>();
+        Iterator<JsonValue> projectsIterator = apiResponse.get("projects").asArray().iterator();
+        while (projectsIterator.hasNext() ) {
+            JsonObject next = (JsonObject) projectsIterator.next();
+            projectUrls.add("https://ec.europa.eu/eipp/desktop/en/projects/project-" + next.get("id") + ".html");
+        }
+
+
+
+        /*Document doc;
         try {
             doc = Jsoup.connect(url)
                     .userAgent("Mozilla")
@@ -90,13 +135,33 @@ public class Scraper extends BaseStep implements StepInterface {
         RowDataUtil.resizeArray(rc, scraperData.getOutputRowInterface().size() );
         rc[scraperData.getOutputRowInterface().size() - 1] = projectsJson.toString();
         putRow(scraperData.getOutputRowInterface(), rc);
-
-//        RowDataUtil.resizeArray(r, scraperData.getOutputRowInterface().size() );
-//        r[scraperData.getOutputRowInterface().size() - 1] = "Hello World!";
-//        putRow(scraperData.getOutputRowInterface(), r);
+*/
+        RowDataUtil.resizeArray(r, scraperData.getOutputRowInterface().size() );
+        r[scraperData.getOutputRowInterface().size() - 1] = getAll(projectUrls);
+        putRow(scraperData.getOutputRowInterface(), r);
 
         return true;
     }
 
+    private String getAll(List<String> projectUrls) {
+        JsonArray projectsJson = new JsonArray();
+        for (int i = 0; i < projectUrls.size(); i++) {
+            JsonObject projectjson = new JsonObject();
+            Document doc = null;
+            try {
+                doc = Jsoup.connect(projectUrls.get(i))
+                        .userAgent("Mozilla")
+                        .timeout(3000)
+                        .get();
+            } catch (IOException e) {
+                logBasic("Could not connect to " + projectUrls.get(i) );
+            }
+            if (doc == null) continue;
+            projectjson.add("pro_name", doc.body().getElementsByTag("h1").get(0).text() );
+
+            projectsJson.add(projectjson);
+        }
+        return projectsJson.toString();
+    }
 
 }
